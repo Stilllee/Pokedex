218dbc71dcdd28010d43b36fb9c65fca6b8d1b90
Stilllee <orodae@gmail.com>
2024-09-24 19:20:55 +0900

    [Day 12] 미션 - 문제

--- src/App.js
+++ src/App.js
@@ -1,1 +1,34 @@
-export default function App() {}
+import PokemonList from "./components/PokemonList.js";
+
+export default function App($app) {
+  const getSearchWord = () => {
+    if (window.location.search.includes("search=")) {
+      return window.location.search.split("search-")[1];
+    }
+    return "";
+  };
+
+  this.state = {
+    type: "",
+    pokemonList: [],
+    searchWord: getSearchWord(),
+    currentPage: window.location.pathname,
+  };
+
+  const pokemonList = new PokemonList({
+    // 코드 작성
+    handleItemClick: () => {},
+    handleTypeClick: () => {},
+  });
+
+  this.setState = (newState) => {
+    this.state = newState;
+    // 코드 작성
+  };
+
+  const init = async () => {
+    // 코드 작성
+  };
+
+  init();
+}
