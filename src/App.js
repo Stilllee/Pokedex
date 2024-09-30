6dda969c38038e8237fca2fab9c1a53977c61a3a
Stilllee <orodae@gmail.com>
2024-09-30 17:36:47 +0900

    [Day 14] PokemonDetail 완성 및 부분렌더링 적용 #4

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import { getPokemonDetail, getPokemonList } from "./modules/api.js";
+
 import Header from "./components/Header.js";
+import PokemonDetail from "./components/PokemonDetail.js";
 import PokemonList from "./components/PokemonList.js";
-import { getPokemonList } from "./modules/api.js";
 
 export default function App($app) {
   const getSearchWord = () => {
@@ -11,74 +13,112 @@ export default function App($app) {
   };
 
   this.state = {
-    type: "",
+    type: window.location.pathname.replace("/", ""),
     pokemonList: [],
     searchWord: getSearchWord(),
     currentPage: window.location.pathname,
   };
 
-  const header = new Header({
-    $app,
-    initialState: {
-      currentPage: this.state.currentPage,
-      searchWord: this.state.searchWord,
-    },
-    handleClick: async () => {
-      history.pushState(null, null, "/");
-      const pokemonList = await getPokemonList();
-      this.setState({
-        ...this.state,
-        pokemonList,
-        type: "",
-        searchWord: getSearchWord(),
-        currentPage: "/",
-      });
-    },
-    //'돋보기 모양'을 누르면 검색 결과를 나타내고, "(기존 url)/?search=searchWord"로 url을 변경하세요.
-    handleSearch: async (searchWord) => {
-      history.pushState(null, null, `?search=${searchWord}`);
-      const searchPokemonList = await getPokemonList(
-        this.state.type,
-        searchWord
-      );
-      this.setState({
-        ...this.state,
-        searchWord,
-        pokemonList: searchPokemonList,
-        currentPage: `?search=${searchWord}`,
-      });
-    },
-  });
+  const renderHeader = () => {
+    new Header({
+      $app,
+      initialState: {
+        currentPage: this.state.currentPage,
+        searchWord: this.state.searchWord,
+      },
+      handleClick: async () => {
+        history.pushState(null, null, "/");
+        const pokemonList = await getPokemonList();
+        this.setState({
+          ...this.state,
+          pokemonList,
+          type: "",
+          searchWord: getSearchWord(),
+          currentPage: "/",
+        });
+      },
+      handleSearch: async (searchWord) => {
+        history.pushState(null, null, `?search=${searchWord}`);
+        const searchPokemonList = await getPokemonList(
+          this.state.type,
+          searchWord
+        );
+        this.setState({
+          ...this.state,
+          searchWord,
+          pokemonList: searchPokemonList,
+          currentPage: `?search=${searchWord}`,
+        });
+      },
+    });
+  };
 
-  const pokemonList = new PokemonList({
-    $app,
-    initialState: this.state.pokemonList,
-    handleItemClick: async (id) => {
-      history.pushState(null, null, `/detail/${id}`);
-      this.setState({
-        ...this.state,
-        currentPage: `/detail/${id}`,
-      });
-    },
-    handleTypeClick: async (type) => {
-      history.pushState(null, null, `/${type}`);
-      const pokemonList = await getPokemonList(type);
-      this.setState({
-        ...this.state,
-        pokemonList,
-        type,
-        currentPage: `/${type}`,
-      });
-    },
-  });
+  const renderPokemonList = () => {
+    new PokemonList({
+      $app,
+      initialState: this.state.pokemonList,
+      handleItemClick: async (id) => {
+        history.pushState(null, null, `/detail/${id}`);
+        this.setState({
+          ...this.state,
+          currentPage: `/detail/${id}`,
+        });
+      },
+      handleTypeClick: async (type) => {
+        history.pushState(null, null, `/${type}`);
+        const pokemonList = await getPokemonList(type);
+        this.setState({
+          ...this.state,
+          pokemonList,
+          type,
+          currentPage: `/${type}`,
+        });
+      },
+    });
+  };
+
+  const renderPokemonDetail = async (pokemonId) => {
+    try {
+      const pokemonDetailData = await getPokemonDetail(pokemonId);
+      new PokemonDetail({ $app, initialState: pokemonDetailData });
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const render = async () => {
+    const path = this.state.currentPage;
+    $app.innerHTML = "";
+    if (!path.startsWith("/detail")) {
+      renderHeader();
+      renderPokemonList();
+    } else {
+      const pokemonId = path.split("/detail/")[1];
+      renderHeader();
+      renderPokemonDetail(pokemonId);
+    }
+  };
 
   this.setState = (newState) => {
     this.state = newState;
-    header.setState({
-      currentPage: this.state.currentPage,
-      searchWord: this.state.searchWord,
-    });
-    pokemonList.setState(this.state.pokemonList);
+    render();
+  };
+
+  const init = async () => {
+    const path = this.state.currentPage;
+    if (!path.startsWith("/detail")) {
+      try {
+        const initialPokemonList = await getPokemonList();
+        this.setState({
+          ...this.state,
+          pokemonList: initialPokemonList,
+        });
+      } catch (error) {
+        console.log(error);
+      }
+    } else {
+      render();
+    }
   };
 
   window.addEventListener("popstate", async () => {
@@ -93,20 +133,9 @@ export default function App($app) {
       type: prevType,
       searchWord: prevSearchWord,
       pokemonList: prevPokemonList,
+      currentPage: urlPath,
     });
   });
-
-  const init = async () => {
-    try {
-      const initialPokemonList = await getPokemonList();
-      this.setState({
-        ...this.state,
-        pokemonList: initialPokemonList,
-      });
-    } catch (error) {
-      console.log(error);
-    }
-  };
 
   init();
 }
